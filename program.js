5407da0aefb41b2f3a52e1e1533fdbaf538f4e07
Ethan Qiu <qethan1996@gmail.com>
2023-04-25 10:25:28 -0400

    starter js

---
+++ program.js
@@ -0,0 +1,13 @@
+var firstName;
+var lastName;
+firstName = "Ethan";
+lastName = "Qiu";
+console.log(" Hello Friend " + firstName + " " + lastName);
+
+var customerId = 555;
+var customerfirstName = "Ethan"
+var customermiddleName = "Nantian"
+var customerlastName = "Qiu"
+console.log(customerId);
+console.log( customerfirstName + " " + customermiddleName + " " + customerlastName)
+

02ee135697aeb24bcd61a7c4b194a8dfc89a62f4
Ethan Qiu <qethan1996@gmail.com>
2023-04-27 13:38:19 -0400

    added stuff

---
+++ exercises/hello_world_website/simple_website/better_website/scripts/greeting.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function showGreeting() {
+  const messageDiv = document.getElementById("messageDiv");
+  console.log(messageDiv);
+
+  messageDiv.innerHTML = "I LOVE MATCHA COOKIEZ"
+
+}
+
+showGreeting();

cb7752ca5a70a5019f29075dfbf8df19f8d53647
Ethan Qiu <qethan1996@gmail.com>
2023-04-27 15:58:11 -0400

    v1 changes

--- exercises/hello_world_website/simple_website/better_website/scripts/greeting.js
+++ exercises/hello_world_website/simple_website/better_website/scripts/greeting.js
@@ -2,8 +2,7 @@
 
 function showGreeting() {
   const messageDiv = document.getElementById("messageDiv");
-  console.log(messageDiv);
-
+ 
   messageDiv.innerHTML = "I LOVE MATCHA COOKIEZ"
 
 }
---
+++ template_literal.js
@@ -0,0 +1,9 @@
+let a = "a";
+let b = "b";
+let c = "c";
+
+let alphabt = a + b + c;
+alphabt = a + "," + b + "," + c;
+alphabt = ` now i know my ${a}, ${b}, ${c}`
+
+console.log(alphabt);

6b9425daecc3add3d56067461632fbba58e1025d
Ethan Qiu <qethan1996@gmail.com>
2023-04-28 10:24:32 -0400

    made an alert on demo page

---
+++ exercises/website_with a buttom/scripts/index_page.js
@@ -0,0 +1,14 @@
+"use strick";
+
+window.onload = init;
+
+function init(){
+    const helloBtm = document.getElementById("helloBtm");
+    helloBtm.onclick = onHelloBtmClicked;
+
+}
+
+function onHelloBtmClicked(){
+
+    alert(" Matcha Doing Friend ")
+}

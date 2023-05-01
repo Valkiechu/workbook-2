6d3bcc9fe48a702222af759787ae17781f65961c
Ethan Qiu <qethan1996@gmail.com>
2023-05-01 11:08:44 -0400

    add items

--- exercises/temperature/script/main.js
+++ exercises/temperature/script/main.js
@@ -6,6 +6,6 @@ const answerField = document.getElementById("answerField");
 
 function convert() {
   let tempinfFiield = tempinfField.vaule;
-  const result = ((tempinfField.vaule - 32) * 5) / 9;
+  const result = (tempinfField.vaule -32) / 1.8;
   answerField.vaule = result;
 }
